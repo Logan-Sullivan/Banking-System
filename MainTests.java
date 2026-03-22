e55582e85c55e715976c32297125247f35feb9a4
MattF555 <mfuller10@missouriwestern.edu>
2026-03-21 23:44:05 -0500

    Added intial tests for the code

---
+++ MainTests.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import  java.io.File;
+import java.util.Scanner;
+
+import org.junit.*;
+
+public class MainTests {
+    @Test 
+    public void testFileExists(){
+        assertDoesNotThrow(() ->{
+            Scanner scanner = new Scanner(new File("src/data.csv"));
+        });
+    }
+
+}
