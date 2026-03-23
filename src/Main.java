8629028265e2aa72809e7fe35f2d0fcff80df801
Logan-Sullivan <lsullivan4@missouriwestern.edu>
2026-03-23 10:35:32 -0500

    Partial implementation of Account, SavingsAccount, customer

--- src/Main.java
+++ src/Main.java
@@ -36,3 +36,56 @@ public class Main {
 
     }
 }
+
+abstract class Account{
+    String accountNumber;
+    private double balance;
+    customer Owner;
+
+    public void deposit(double amount){
+        System.out.println("Successfully Deposited "+ amount);
+        System.out.print(this.balance);
+        this.balance += amount;
+        System.out.print(" → "+ this.balance+"\n");
+    }
+    public void withdraw(double amount){}
+    public double getBalance(){
+        return this.balance;
+    }
+}
+
+class SavingsAccount extends Account{
+    double intrestRate;
+    String compoundFreq;
+    boolean overdraftBackup;
+
+    public SavingsAccount(double intrestRate, String compoundFreq, boolean overdraftBackup,double balance){
+        this.intrestRate = intrestRate;
+        this.compoundFreq = compoundFreq;
+        this.overdraftBackup = overdraftBackup;
+        this.deposit(balance);
+    }
+
+    public void applyDailyInterest(){
+        System.out.println("Daily interest applied: " + this.getBalance());
+        this.deposit(this.getBalance()*intrestRate);
+        System.out.print(" → "+ this.getBalance() +"\n");
+    }
+
+    public void toggleOverdraftBackup(){
+        this.overdraftBackup = !this.overdraftBackup; // toggles between true and false
+        if (this.overdraftBackup) {
+            System.out.println("Account set as backup");
+        } else {
+            System.out.println("Account set as non-backup");
+        }
+    }
+
+}
+
+class customer{
+    String customerId;
+    String firstName;
+    String lastName;
+
+}
