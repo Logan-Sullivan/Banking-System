8a556fedc22ce78c36d1414577dcafa231b0034a
Logan-Sullivan <lsullivan4@missouriwestern.edu>
2026-04-07 13:21:46 -0500

    Added default constructor to generate a randomized account for testing and added javadoc comment to constructor

--- src/Account_Classes/SavingsAccount.java
+++ src/Account_Classes/SavingsAccount.java
@@ -1,4 +1,4 @@
-package src.Account_Classes;
+package Account_Classes;
 import java.util.UUID;
 
 public class SavingsAccount extends Account{
@@ -6,6 +6,23 @@ public class SavingsAccount extends Account{
     String compoundFreq;
     boolean overdraftBackup;
 
+
+    //default constructor for testing
+    public SavingsAccount(){
+        this.accountNumber = UUID.randomUUID().toString();;
+        this.intrestRate = .01+(99.99-.01)*Math.random();
+        this.compoundFreq = "31";
+        this.overdraftBackup = true;
+        this.deposit((int)(Math.random()*1000));
+    }
+
+    /**
+     *
+     * @param intrestRate Rate of interest in percentage ie: 25.6 = %25.6 <br>expected value should be above 0% and below 100%<br>
+     * @param compoundFreq Rate at which interest is accrued in days expected value 1-365<br>
+     * @param overdraftBackup Weather or not account is used for overdraft bacup<br>
+     * @param balance The balance of the savings account<br>
+     */
     public SavingsAccount(double intrestRate, String compoundFreq, boolean overdraftBackup,double balance){
         this.accountNumber = UUID.randomUUID().toString();
         this.intrestRate = intrestRate;
